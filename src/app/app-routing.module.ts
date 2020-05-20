79bb3a006ecd4776e69d6d7cffdd63000aa467e1
vijay0405 <uvijaysimha@gmail.com>
2020-05-21 00:02:26 +0530

    fixed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router"
+import { LoginComponent } from "./auth/login/login.component"
+import { SignUpComponent } from './auth/signUp/signUp.component';
+import { AuthGuard } from './auth/auth.guard'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent },
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignUpComponent},
+  // {path: 'create', component: comp1, canActivate: [AuthGuard]},
+  // {path: 'edit/:comp', component: comp2, canActivate: [AuthGuard]}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
